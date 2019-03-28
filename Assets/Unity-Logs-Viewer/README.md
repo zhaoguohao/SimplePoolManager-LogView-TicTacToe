483b1f6833b2a67828cb879e7d38f44a8a780c98
paul <zhaoguohao97@sina.com>
2019-03-28 15:25:31 +0800

    fist

---
+++ Assets/Unity-Logs-Viewer/README.md
@@ -0,0 +1,17 @@
+# Unity-Logs-Viewer
+只需一步，就能在运行时查看Console的Log！
+
+
+准备步骤：
+1、通过菜单Reporter->Create在第一个场景创建所需物体
+2、将Reporter.cs的执行顺序优先级调到最高” Scrip execution order ” in (Edit -> Project Settings )
+
+使用方法：
+使用鼠标左键按下或者手指在屏幕上画一个圈，会弹出log界面
+
+
+遇到问题可至“洪流学堂”公众号反馈。
+
+由于原作者已经不维护该插件，由我维护的该插件的最新github地址：https://github.com/zhenghongzhi/Unity-Logs-Viewer
+
+感谢原作者：https://github.com/aliessmael/Unity-Logs-Viewer
---
+++ Assets/Unity-Logs-Viewer/Reporter/Editor/WelcomeScreen.cs
@@ -0,0 +1,61 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[InitializeOnLoad]
+public class Appload
+{
+    static Appload()
+    {
+        bool hasKey = PlayerPrefs.HasKey("zgh");
+        if (hasKey == false)
+        {
+            PlayerPrefs.SetInt("zgh", 1);
+            WelcomeScreen.ShowWindow();
+        }
+    }
+}
+
+public class WelcomeScreen : EditorWindow
+{
+    private Texture mSamplesImage;
+    private Rect imageRect = new Rect(30f, 90f, 350f, 350f);
+    private Rect textRect = new Rect(15f, 15f, 380f, 100f);
+
+    public void OnEnable()
+    {
+        mSamplesImage = LoadTexture("zgh.png");
+    }
+
+
+    Texture LoadTexture(string name)
+    {
+        string path = "Assets/Unity-Logs-Viewer/Reporter/Editor/";
+        return (Texture)AssetDatabase.LoadAssetAtPath(path + name, typeof(Texture));
+    }
+
+    public void OnGUI()
+    {
+        GUIStyle style = new GUIStyle
+        {
+            fontSize = 14,
+            normal = { textColor = Color.green }
+        };
+
+        if (mSamplesImage == null)
+        {
+            mSamplesImage = LoadTexture("zgh.jpg");
+        }
+
+        GUI.Label(textRect, "如果该项目对你有用，记得帮我点一下Star哟\n这个页面只会显示一次奥", style);
+        GUI.DrawTexture(imageRect, mSamplesImage);
+    }
+
+    public static void ShowWindow()
+    {
+        WelcomeScreen window = GetWindow<WelcomeScreen>(true, "本项目仅用于学习交流，如有侵权请及时联系我昂");
+        window.minSize = window.maxSize = new Vector2(410f, 470f);
+        DontDestroyOnLoad(window);
+    }
+}
+
+
